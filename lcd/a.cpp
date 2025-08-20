c277b92f411a136870cbfa19c16b72c52f8198bc
Bao Dang <dang.hbaokody@gmail.com>
2025-08-20 19:46:32 +0700

    up Wed Aug 20 12:46:32 PM UTC 2025

--- lcd/a.cpp
+++ lcd/a.cpp
@@ -2,54 +2,7 @@
 
 using namespace std;
 
-using ll = long long;
-using vi = vector<int>;
-using vl = vector<ll>;
-using vvi = vector<vi>;
-using vvl = vector<vl>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using mii = map<int, int>;
-using mci = map<char, int>;
-using vpii = vector<pii>;
-
-#define fi first
-#define se second
-#define pb push_back
-#define eb emplace_back
-#define ins insert
-#define mp make_pair
-#define mtp make_tuple
-#define lb lower_bound
-#define ub upper_bound
-
-#define sz(v) (int)(v).size()
-#define all(v) v.begin(), v.end()
-#define par(v, n) v.begin(), v.begin() + n
-
-#define loop(i, s, e, j) \
-	for (auto i = (s); ((j) > 0) ? (i <= (e)) : (i >= (e)); i += (j))
-#define iter(i, n) range(i, 0, n - 1, 1)
-#define each(i, arr) for (auto &i : (arr))
-
-constexpr double EPS [[maybe_unused]] = 1e-9;
-constexpr double PI [[maybe_unused]] = 3.14159265358979323846;
-constexpr int MOD [[maybe_unused]] = static_cast<int>(1e9 + 7);
-constexpr int INF [[maybe_unused]] = INT_MAX;
-constexpr ll LINF [[maybe_unused]] = LLONG_MAX;
-constexpr char nl [[maybe_unused]] = '\n';
-
-// --------------------------------------------------------------------------
-
-template <class T> using max_heap = priority_queue<T>;
-template <class T> using min_heap = priority_queue<T, vector<T>, greater<T>>;
-
-string to_upper(string a) {
-	return transform(a.begin(), a.end(), a.begin(), ::toupper), a;
-}
-string to_lower(string a) {
-	return transform(a.begin(), a.end(), a.begin(), ::tolower), a;
-}
+constexpr char nl = '\n';
 
 // --------------------------------------------------------------------------
 // Data structures (only Leetcode)
@@ -135,9 +88,23 @@ template <typename T, typename V> void __info(const pair<T, V> &x);
 template <typename T, typename V, typename K>
 void __info(const tuple<T, V, K> &x);
 
-void __info(int x) { cerr << x; }
+void __info(int x) {
+	if (x == INT_MIN)
+		cerr << "-I";
+	else if (x == INT_MAX)
+		cerr << "I";
+	else
+		cerr << x;
+}
 void __info(long x) { cerr << x; }
-void __info(long long x) { cerr << x; }
+void __info(long long x) {
+	if (x == INT_MIN || x == LLONG_MIN)
+		cerr << "-I";
+	else if (x == INT_MAX || x == LLONG_MAX)
+		cerr << "I";
+	else
+		cerr << x;
+}
 void __info(unsigned x) { cerr << x; }
 void __info(unsigned long x) { cerr << x; }
 void __info(unsigned long long x) { cerr << x; }
@@ -148,6 +115,13 @@ void __info(char x) { cerr << '\'' << x << '\''; }
 void __info(const char *x) { cerr << '\"' << x << '\"'; }
 void __info(const string &x) { cerr << '\"' << x << '\"'; }
 void __info(bool x) { cerr << (x ? "true" : "false"); }
+
+void __info(const vector<bool> &x) {
+	int f = 0;
+	cerr << '{';
+	for (auto i : x) cerr << (f++ ? ", " : ""), __info(i);
+	cerr << "}";
+}
 
 template <typename T> void __info(const T &x) {
 	int f = 0;
@@ -185,9 +159,64 @@ template <typename T, typename... V> void __print(T t, V... v) {
 #define info(x...)
 #endif
 
+// --------------------------------------------------------------------------
+// Runner (only Leetcode)
+
+vector<int> arr [[maybe_unused]], nums [[maybe_unused]];
+vector<string> strs [[maybe_unused]];
+vector<vector<int>> grid [[maybe_unused]];
+vector<vector<string>> strss [[maybe_unused]];
+
+int v [[maybe_unused]], val [[maybe_unused]];
+long long l [[maybe_unused]], lg [[maybe_unused]];
+double d [[maybe_unused]], dd [[maybe_unused]];
+char c [[maybe_unused]], ch [[maybe_unused]];
+string s [[maybe_unused]], str [[maybe_unused]];
+
+template <typename Obj, typename MemFn, typename... Args>
+void perform(Obj &&obj, MemFn &&memfn, Args &&...args) {
+	__read(args...);
+	if constexpr (std::is_void_v<decltype((std::forward<Obj>(obj).*memfn)(
+					  std::forward<Args>(args)...))>) {
+		(std::forward<Obj>(obj).*memfn)(std::forward<Args>(args)...);
+	} else {
+		auto result =
+			(std::forward<Obj>(obj).*memfn)(std::forward<Args>(args)...);
+		__info(result);
+		cerr << nl;
+	}
+}
+
 // **************************************************************************
 
-void solve(int test_case [[maybe_unused]]) {}
+class Solution {
+  public:
+	int findMaxForm(vector<string> &strs, int m, int n) {
+		vector<vector<int>> f(n + 1, vector<int>(m + 1)), nf = f;
+		for (string &s : strs) {
+			int b0 = 0, b1 = 0;
+			for (char c : s) {
+				if (c == '0')
+					b0++;
+				else
+					b1++;
+			}
+			for (int i = b1; i <= n; i++) {
+				for (int j = b0; j <= m; j++) {
+					nf[i][j] = max(f[i][j], f[i - b1][j - b0] + 1);
+				}
+			}
+			f = nf;
+		}
+		return f[n][m];
+	}
+};
+
+// **************************************************************************
+
+void solve(int test_case [[maybe_unused]]) {
+	perform(Solution(), &Solution::findMaxForm, strs, v, val);
+}
 
 // **************************************************************************
 
