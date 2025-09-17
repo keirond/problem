ee8cfda53c7411a736f845f67ec8a333375bcecb
Keiron Dang <dang.hbaokody@gmail.com>
2025-09-17 09:39:14 +0700

    up Wed Sep 17 02:39:14 AM UTC 2025

--- cfs/done/626E.cpp
+++ cfs/done/626E.cpp
@@ -18,26 +18,25 @@ void solve(int test_case [[maybe_unused]]) {
     sort(begin(nums), end(nums));
 
     using ll = long long;
-    using ld = long double;
 
     vector<ll> ps(n + 1);
     for (int i = 0; i < n; i++) { ps[i + 1] = ps[i] + nums[i]; }
 
-    ld mx = INT_MIN;
+    double mx = INT_MIN;
     vector<int> info{0, 0, 0};
 
     for (int i = 0; i < n; i++) {
         auto calc = [&](int m) {
-            ld mean = (ld)(ps[n] - ps[n - m] + ps[i + 1] - ps[i - m]) /
-                      (2 * m + 1);
+            double mean = (double)(ps[n] - ps[n - m] + ps[i + 1] - ps[i - m]) /
+                          (2 * m + 1);
             return mean - nums[i];
         };
         int l = 0, r = min(i, n - i - 1);
         while (r - l > 3) {
             int m1 = l + (r - l) / 3;
             int m2 = r - (r - l) / 3;
-            ld t1 = calc(m1);
-            ld t2 = calc(m2);
+            double t1 = calc(m1);
+            double t2 = calc(m2);
             if (t1 < t2) {
                 l = m1 + 1;
             } else {
@@ -45,9 +44,9 @@ void solve(int test_case [[maybe_unused]]) {
             }
         }
         int best = l;
-        ld t1 = calc(best);
+        double t1 = calc(best);
         for (int i = l; i <= r; i++) {
-            ld t2 = calc(i);
+            double t2 = calc(i);
             if (t2 > t1) { best = i, t1 = t2; }
         }
         if (t1 > mx) { mx = t1, info = {i, i, best}; }
@@ -55,16 +54,16 @@ void solve(int test_case [[maybe_unused]]) {
 
     for (int i = 1; i < n; i++) {
         auto calc = [&](int m) {
-            ld mean = (ld)(ps[n] - ps[n - m] + ps[i + 1] - ps[i - 1 - m]) /
-                      (2 * m + 2);
-            return mean - (ld)(nums[i - 1] + nums[i]) / 2;
+            double mean = (double)(ps[n] - ps[n - m] + ps[i + 1] - ps[i - 1 - m]) /
+                          (2 * m + 2);
+            return mean - (double)(nums[i - 1] + nums[i]) / 2;
         };
         int l = 0, r = min(i - 1, n - i - 1);
         while (r - l > 3) {
             int m1 = l + (r - l) / 3;
             int m2 = r - (r - l) / 3;
-            ld t1 = calc(m1);
-            ld t2 = calc(m2);
+            double t1 = calc(m1);
+            double t2 = calc(m2);
             if (t1 < t2) {
                 l = m1 + 1;
             } else {
@@ -72,9 +71,9 @@ void solve(int test_case [[maybe_unused]]) {
             }
         }
         int best = l;
-        ld t1 = calc(best);
+        double t1 = calc(best);
         for (int i = l; i <= r; i++) {
-            ld t2 = calc(i);
+            double t2 = calc(i);
             if (t2 > t1) { best = i, t1 = t2; }
         }
         if (t1 > mx) { mx = t1, info = {i, i, best}; }
