b5daf35542058588507e179f8c92314cb3290956
Keiron Dang <dang.hbaokody@gmail.com>
2025-09-21 16:26:55 +0700

    up Sun Sep 21 09:26:55 AM UTC 2025

--- lcd/a.cpp
+++ lcd/a.cpp
@@ -178,42 +178,52 @@ void perform(Obj &&obj, MemFn memfn, Args &&...args) {
 // * START ******************************************************************
 
 /**
- * Definition for singly-linked list.
- * struct ListNode {
+ * Definition for a binary tree node.
+ * struct TreeNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
-class Solution {
+class FindElements {
+    TreeNode *root;
+
 public:
 
-    int pairSum(ListNode *head) {
-        ListNode *slow = head, *fast = head;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+    FindElements(TreeNode *root) : root(root) { root->val = 0; }
+
+    bool find(int target) {
+        if (target == 0) { return true; }
+        vector<int> direction;
+        while (target) {
+            target--;
+            direction.push_back(target & 1);
+            target >>= 1;
         }
 
-        ListNode *prev = nullptr, *curr = head, *next = head->next;
-        while (curr != slow) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        TreeNode *cur = root;
+        for (int i = direction.size() - 1; i >= 0; i--) {
+            if (direction[i]) {
+                if (!cur->right) { return false; }
+                cur = cur->right;
+            } else {
+                if (!cur->left) { return false; }
+                cur = cur->left;
+            }
         }
-
-        int ans = INT_MIN;
-        while (slow && prev) {
-            ans = max(ans, slow->val + prev->val);
-            slow = slow->next;
-            prev = prev->next;
-        }
-        return ans;
+        return true;
     }
 };
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
 
 // * END ********************************************************************
 
