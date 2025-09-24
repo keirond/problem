9f28878dfb25119cb913bb9bf0a365573a1fa4f2
Crix Dang <crix.dang@gmail.com>
2025-09-24 10:40:44 +0700

    up Wed Sep 24 03:40:44 AM UTC 2025

--- lcd/a.cpp
+++ lcd/a.cpp
@@ -182,22 +182,46 @@ public:
 
     bool hasSameDigits(string s) {
         int n = s.size();
-        vector<int> nums(n);
-        for (int i = 0; i < n; i++) { nums[i] = s[i] - '0'; }
-        while (n > 2) {
-            for (int i = 0; i < n - 1; i++) {
-                nums[i] = (nums[i] + nums[i + 1]) % 10;
+
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int a = n - 2, b = i;
+            int t1 = 1;
+            while (b) {
+                int x = a % 2, y = b % 2;
+                if (y > x) {
+                    t1 = 0;
+                    break;
+                }
+                a /= 2, b /= 2;
             }
-            n--;
+
+            a = n - 2, b = i;
+            vector<int> fact{1, 1, 2, 1, 4};
+            int t2 = 1;
+            while (b) {
+                int x = a % 5, y = b % 5;
+                if (y > x) {
+                    t2 = 0;
+                    break;
+                }
+                int t = 1LL * fact[x] * fact[y] * fact[y] * fact[y] *
+                        fact[x - y] * fact[x - y] * fact[x - y] % 5;
+                t2 = 1LL * t2 * t % 5;
+                a /= 5, b /= 5;
+            }
+
+            int t3 = (t1 * 5 + t2 * 6) % 10;
+            ans = (ans + t3 * (s[i] - s[i + 1])) % 10;
         }
-        return nums[0] == nums[1];
+        return !ans;
     }
 };
 
 // * END ********************************************************************
 
 void solve(int test_case [[maybe_unused]]) {
-    perform(Solution(), &Solution::waysToFillArray, grid);
+    perform(Solution(), &Solution::hasSameDigits, s);
 }
 
 // **************************************************************************
