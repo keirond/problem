2b3f6e6f3eccb49d8ada87004b09147d1c2d4cca
Crix Dang <crix.dang@gmail.com>
2025-09-25 16:36:34 +0700

    up Thu Sep 25 09:36:34 AM UTC 2025

--- lcd/a.cpp
+++ lcd/a.cpp
@@ -182,13 +182,21 @@ public:
 
     long long makeSubKSumEqual(vector<int> &arr, int k) {
         int n = arr.size();
-        vector<long long> sms(n);
-        for (int i = 0; i < k; i++) { sms[0] += arr[i]; }
-        for (int i = 1; i < n; i++) {
-            sms[i] = sms[i - 1] + arr[(i + k - 1) % n] - arr[i - 1];
+        int g = gcd(n, k);
+        long long ans = 0;
+        for (int i = 0; i < g; i++) {
+            vector<int> temp;
+            for (int j = i; j < n; j += g) { temp.push_back(arr[j]); }
+            sort(begin(temp), end(temp));
+
+            int m = temp.size();
+            int mid =
+                    (m % 2) ? temp[m / 2] : (temp[m / 2] + temp[m / 2 - 1]) / 2;
+            for (int i = 0; i < m; i++) {
+                ans += abs((long long)temp[i] - mid);
+            }
         }
-        info(sms);
-        return 0;
+        return ans;
     }
 };
 
