07c837e69a1f636a787f36a248c3fcd58e00dd58
Crix Dang <crix.dang@gmail.com>
2025-10-03 10:45:04 +0700

    up Fri Oct  3 03:45:04 AM UTC 2025

--- lcd/a.cpp
+++ lcd/a.cpp
@@ -180,65 +180,79 @@ void perform(Obj &&obj, MemFn memfn, Args &&...args) {
 class Solution {
 public:
 
-    vector<vector<int>> min_tr, max_tr;
-    vector<int> lg;
-
-    long long maxTotalValue(vector<int> &nums, int k) {
-        int n = nums.size();
-        int K = __lg(n) + 1;
-
-        min_tr.assign(K, vector<int>(n));
-        max_tr.assign(K, vector<int>(n));
-        lg.resize(n + 1);
-        for (int i = 2; i <= n; i++) { lg[i] = lg[i / 2] + 1; }
-        for (int i = 0; i < n; i++) {
-            min_tr[0][i] = nums[i];
-            max_tr[0][i] = nums[i];
+    int slidingPuzzle(vector<vector<int>> &board) {
+        int k = 0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) { k = k * 10 + board[i][j]; }
         }
-        for (int j = 1; j < K; j++) {
-            for (int i = 0; i + (1 << (j - 1)) < n; i++) {
-                min_tr[j][i] = min(min_tr[j - 1][i],
-                                   min_tr[j - 1][i + (1 << (j - 1))]);
-                max_tr[j][i] = max(max_tr[j - 1][i],
-                                   max_tr[j - 1][i + (1 << (j - 1))]);
+        int s = 123450;
+
+        int ans = 0;
+        deque<int> dq, ndq;
+        vector<int> vt(543211);
+        dq.push_back(k);
+        vt[k] = 1;
+
+        auto convert = [&](vector<int> &t, int i, int j) {
+            swap(t[i], t[j]);
+            int ans = 0;
+            for (int i = 0; i < 6; i++) { ans = ans * 10 + t[i]; }
+            swap(t[i], t[j]);
+            if (!vt[ans]) {
+                vt[ans] = 1;
+                ndq.push_back(ans);
             }
+        };
+
+        while (!dq.empty()) {
+            ndq.clear();
+            for (int d : dq) {
+                if (d == s) { return ans; }
+                vector<int> temp(6);
+                int j = 0;
+                for (int i = 5; i >= 0; i--) {
+                    temp[i] = d % 10;
+                    if (!temp[i]) { j = i; }
+                    d /= 10;
+                }
+                if (j == 0) {
+                    convert(temp, 0, 1);
+                    convert(temp, 0, 3);
+                }
+                if (j == 1) {
+                    convert(temp, 1, 0);
+                    convert(temp, 1, 2);
+                    convert(temp, 1, 4);
+                }
+                if (j == 2) {
+                    convert(temp, 2, 1);
+                    convert(temp, 2, 5);
+                }
+                if (j == 3) {
+                    convert(temp, 3, 0);
+                    convert(temp, 3, 4);
+                }
+                if (j == 4) {
+                    convert(temp, 4, 1);
+                    convert(temp, 4, 3);
+                    convert(temp, 4, 5);
+                }
+                if (j == 5) {
+                    convert(temp, 5, 4);
+                    convert(temp, 5, 2);
+                }
+            }
+            dq = ndq;
+            ans++;
         }
-
-        priority_queue<pair<long long, pair<int, int>>> pq;
-        for (int i = 0; i < n; i++) {
-            pq.push({(long long)query_max(i, n - 1) - query_min(i, n - 1),
-                     {i, n - 1}});
-        }
-
-        long long ans = 0;
-        for (int j = 0; j < k; j++) {
-            if (pq.empty()) { break; }
-            auto &d = pq.top();
-            ans += d.first;
-            auto [l, r] = d.second;
-            pq.pop();
-            if (l >= r) { continue; }
-            pq.push({(long long)query_max(l, r - 1) - query_min(l, r - 1),
-                     {l, r - 1}});
-        }
-        return ans;
-    }
-
-    int query_min(int l, int r) {
-        int i = lg[r - l + 1];
-        return min(min_tr[i][l], min_tr[i][r - (1 << i) + 1]);
-    }
-
-    int query_max(int l, int r) {
-        int i = lg[r - l + 1];
-        return max(max_tr[i][l], max_tr[i][r - (1 << i) + 1]);
+        return -1;
     }
 };
 
 // * END ********************************************************************
 
 void solve(int test_case [[maybe_unused]]) {
-    perform(Solution(), &Solution::maxTotalValue, nums, v);
+    perform(Solution(), &Solution::slidingPuzzle, grid);
 }
 
 // **************************************************************************
