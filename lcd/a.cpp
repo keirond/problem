b7ced30d0175aa8a0e8d19fe5484c272fe4682cf
Crix Dang <crix.dang@gmail.com>
2025-10-04 07:40:00 +0700

    up Sat Oct  4 12:40:00 AM UTC 2025

--- lcd/a.cpp
+++ lcd/a.cpp
@@ -179,26 +179,34 @@ void perform(Obj &&obj, MemFn memfn, Args &&...args) {
 
 class Solution {
 public:
-    int countStableSubsequences(vector<int>& nums) {
-        int mod=1e9+7;
-        int n=nums.size();
-        vector<long long> f{0, 0, 1, 0, 0}, nf;
-        for(int i=0; i<n; i++) {
-            nf.assign(5, 0);
-            if(nums[i]%2) {
-                nf[0] = f[1] + 1;
-                nf[1] = f[2] + 1;
-            } else {
 
+    int countStableSubsequences(vector<int> &nums) {
+        int mod = 1e9 + 7;
+        int n = nums.size();
+        vector<long long> f{0, 0, 1, 0, 0}, nf;
+        for (int i = 0; i < n; i++) {
+            nf = f;
+            if (nums[i] % 2) {
+                nf[0] = (f[1] + nf[0]) % mod;
+                nf[1] = ((f[2] + f[3]) % mod + (f[4] + nf[1]) % mod) % mod;
+            } else {
+                nf[3] = ((f[0] + f[1]) % mod + (f[2] + nf[3]) % mod) % mod;
+                nf[4] = (f[3] + nf[4]) % mod;
             }
+            f = nf;
         }
+        long long ans = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i != 2) { ans = (ans + f[i]) % mod; }
+        }
+        return ans;
     }
 };
 
 // * END ********************************************************************
 
 void solve(int test_case [[maybe_unused]]) {
-    perform(Solution(), &Solution::subsequenceSumAfterCapping, nums, v);
+    perform(Solution(), &Solution::countStableSubsequences, nums);
 }
 
 // **************************************************************************
