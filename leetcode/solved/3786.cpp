f8e60a53dd84868487af1f8c394747038db7f9f4
Keiron <keiron.dang@gmail.com>
2026-01-05 12:57:13 +0700

    up Mon Jan  5 05:57:13 AM UTC 2026

--- leetcode/solved/3786.cpp
+++ leetcode/solved/3786.cpp
@@ -5,22 +5,54 @@ class Solution {
 public:
 
     vector<vector<int>> adj;
-    vector<bool> vis;
+    vector<int> grp;
 
-    long long dfs(int u) {
+    vector<bool> vis;
+    vector<long long> f;
+    vector<unordered_map<int, pair<int, long long>>> g;
+
+    void dfs(int u) {
+        long long &ans = f[u];
+        unordered_map<int, pair<int, long long>> &freq = g[u];
+
+        freq[grp[u]].first++;
         for (int v : adj[u]) {
-            if (!vis[v]) { dfs(v); }
+            if (!vis[v]) {
+                vis[v] = true;
+                dfs(v);
+                ans += f[v];
+                for (auto [k, v1] : g[v]) {
+                    long long t = v1.second + v1.first;
+                    ans += t * freq[k].first;
+                    ans += 1LL * v1.first * freq[k].second;
+                    freq[k].first += v1.first;
+                    freq[k].second += t;
+                }
+            }
         }
     }
 
-    long long interactionCosts(int n, vector<vector<int>> &edges,
-                               vector<int> &group) {
+    long long interactionCosts(int n, const vector<vector<int>> &edges,
+                               const vector<int> &group) {
         adj.resize(n);
         for (auto &e : edges) {
             int u = e[0], v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vis.resize(n);
+
+        grp = group;
+        vis.assign(n, false);
+        f.resize(n);
+        g.resize(n);
+        vis[0] = true;
+        dfs(0);
+        return f[0];
     }
 };
+
+int main() {
+    Solution s;
+    cout << s.interactionCosts(3, {{0, 1}, {1, 2}}, {3, 2, 3}) << endl;
+    return 0;
+}
